a7d9ec13adafbbade7023631b0c0b2c53d0d7a86
karthikknuware <karthik.koppuravuri@nuware.com>
2016-04-06 12:08:11 +0530

    Update test.rb

--- logly/recipes/test.rb
+++ logly/recipes/test.rb
@@ -1,6 +1,10 @@
-directory '/tmp/hell3' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
+#directory '/tmp/hell3' do
+  #owner 'root'
+  #group 'root'
+  #mode '0755'
+  #action :create
+#end
+bash 'install-ruby' do
+  code <<-EOH
+ 
+    EOH
