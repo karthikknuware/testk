985f6eac1dba34c8bb87263306099faea8efa2fe
karthikknuware <karthik.koppuravuri@nuware.com>
2016-04-06 10:30:21 +0530

    ruby

--- logly/recipes/default.rb
+++ logly/recipes/default.rb
@@ -1,13 +1,26 @@
-directory '/tmp/hello2' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
+package 'Install-ruby' do
+ package_name [‘gcc-c++ ’, ‘patch’, ‘readline’, ‘readline-devel’, ‘zlib’, ‘zlib-devel’, ‘libyaml-devel’, ‘libffi-devel’, ‘openssl-devel’, ‘make’, ‘bzip2’, ‘autoconf’, ‘automake’, ‘libtool’, ‘bison’, ‘iconv-devel’, ‘sqlite-devel’]
+  action :install
 end
-#execute 'apache_install' do
+
+bash 'install-ruby' do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+ curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+ curl -L get.rvm.io | bash -s stable
+ source /etc/profile.d/rvm.sh
+ rvm reload
+ rvm requirements run
+ rvm install 2.3.0
+    EOH
+end
+
+#execute 'ruby_install' do
  # command 'sudo yum install httpd'
 #end
-
-package 'Install Apache' do
- package_name 'httpd'
-end
+#directory '/tmp/hello2' do
+ # owner 'root'
+ # group 'root'
+  #mode '0755'
+  #action :create
+#end
