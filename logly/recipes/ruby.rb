1fab4ce135c8b68ad62ab0b9b8d83f292686f293
karthikknuware <karthik.koppuravuri@nuware.com>
2016-04-07 16:00:06 +0530

    updated

--- logly/recipes/ruby.rb
+++ logly/recipes/ruby.rb
@@ -1,19 +1,17 @@
 bash 'install-ruby' do
   code <<-EOH
- sudo apt-get install gcc-c++ patch readline readline-devel zlib zlib-devel 
- sudo apt-get install libyaml-devel libffi-devel openssl-devel make 
- sudo apt-get install bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
- sudo curl -sSL https://rvm.io/mpapis.asc | gpg --import -
- sudo curl -L get.rvm.io | bash -s stable
- sudo source /home/ubuntu/.rvm/scripts/rvm 
- sudo rvm reload
- sudo rvm requirements run
- sudo rvm install 2.3.0
- sudo apt-get update
- sudo rvm use ruby-2.3.0@rails4.2 --create
- sudo gem install rails --version=4.2.6
- sudo apt-get update
- sudo gem install unicorn
- sudo apt-get update
+  apt-get install gcc-c++ patch readline readline-devel zlib zlib-devel 
+  apt-get install libyaml-devel libffi-devel openssl-devel make 
+  apt-get install bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
+  curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+  curl -L get.rvm.io | bash -s stable
+  source /home/ubuntu/.rvm/scripts/rvm 
+ rvm reload
+ rvm requirements run
+ rvm install 2.3.0
+ rvm use ruby-2.3.0@rails4.2 --create
+ gem install rails --version=4.2.6
+ gem install unicorn
+ apt-get update
     EOH
 end
