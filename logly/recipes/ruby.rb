36ba8525f934690958e8518fbdb455be83a75997
karthikknuware <karthik.koppuravuri@nuware.com>
2016-04-08 16:54:14 +0530

    ubuntu

--- logly/recipes/ruby.rb
+++ logly/recipes/ruby.rb
@@ -1,18 +1,15 @@
 bash 'install-ruby' do
   code <<-EOH
-  sudo su
-  apt-get install gcc-c++ patch readline readline-devel zlib zlib-devel 
-  apt-get install libyaml-devel libffi-devel openssl-devel make 
-  apt-get install bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
   curl -sSL https://rvm.io/mpapis.asc | gpg --import -
   curl -L get.rvm.io | bash -s stable
   source /home/ubuntu/.rvm/scripts/rvm 
  rvm reload
  rvm requirements run
  rvm install 2.3.0
+ rvm use 2.3.0 --default
  rvm use ruby-2.3.0@rails4.2 --create
  gem install rails --version=4.2.6
  gem install unicorn
- apt-get update
-    EOH
+ rvm use ruby-2.3.0@rails4.2 --create
+   EOH
 end
